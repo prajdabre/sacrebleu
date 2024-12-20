2e606377623b7bc5bb8ecd16517ef5afab48505c
draj <raj.dabre@nict.go.jp>
2024-12-20 13:33:28 +0900

    Master

--- bytf-cmds.sh
+++ bytf-cmds.sh
@@ -4,8 +4,15 @@ cat IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.translation | sacrebleu IndicM
 
 cat IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.translation | sacrebleu IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.ref -m bytf --sentence-level | rev | cut -d " " -f1 | rev > IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.bytf
 
+cat IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.translation | sacrebleu IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.ref -m chrf --chrf-char-order 6 --chrf-word-order 2  --sentence-level | rev | cut -d " " -f1 | rev > IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.chrf.char_order_6_word_order_2
+
+cat IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.translation | sacrebleu IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.ref -m bytf --bytf-byte-order 10 --chrf-char-order 6 --chrf-word-order 2 --sentence-level | rev | cut -d " " -f1 | rev > IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.bytf.byte_order_10_char_order_6_word_order_2
+
 python normalize_scores.py IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.chrf 100
 python normalize_scores.py IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.bytf 100
+
+python normalize_scores.py IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.chrf.char_order_6_word_order_2 100
+python normalize_scores.py IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.bytf.byte_order_10_char_order_6_word_order_2 100
 
 python compute_correlation_metrics.py IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.chrf.normalized IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.da_norm_score
 
@@ -13,5 +20,14 @@ python compute_correlation_metrics.py IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_t
 
 python compute_correlation_metrics.py IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.chrf.normalized IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.mqm_norm_score
 
-
 python compute_correlation_metrics.py IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.bytf.normalized IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.mqm_norm_score
+
+## Now all 4 commands for the scores with chrf and bytf with different parameters are ready to be run.
+
+python compute_correlation_metrics.py IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.chrf.char_order_6_word_order_2.normalized IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.da_norm_score
+
+python compute_correlation_metrics.py IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.bytf.byte_order_10_char_order_6_word_order_2.normalized IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.da_norm_score
+
+python compute_correlation_metrics.py IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.chrf.char_order_6_word_order_2.normalized IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.mqm_norm_score
+
+python compute_correlation_metrics.py IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.bytf.byte_order_10_char_order_6_word_order_2.normalized IndicMT-Eval/Dataset/Indic\ MT\ Eval/Hin_test.mqm_norm_score
